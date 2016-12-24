0c30fc098bec6cfaa58192817d976bb17a672b14
Rahul MahendraKumar <rahulmsinghvi@gmail.com>
2016-12-24 21:54:13 +0530

    Improving readibility

--- DataFiles/Week2/Python program for video examples (Module 2).py
+++ DataFiles/Week2/Python program for video examples (Module 2).py
@@ -22,44 +22,44 @@ data['AGE'] = data['AGE'].convert_objects(convert_numeric=True)
 
 #counts and percentages (i.e. frequency distributions) for each variable
 c1 = data['TAB12MDX'].value_counts(sort=False)
-print (c1)
+print(c1)
 
 p1 = data['TAB12MDX'].value_counts(sort=False, normalize=True)
-print (p1)
+print(p1)
 
 c2 = data['CHECK321'].value_counts(sort=False)
 print(c2)
 
 p2 = data['CHECK321'].value_counts(sort=False, normalize=True)
-print (p2)
+print(p2)
 
 c3 = data['S3AQ3B1'].value_counts(sort=False)
 print(c3)
 
 p3 = data['S3AQ3B1'].value_counts(sort=False, normalize=True)
-print (p3)
+print(p3)
 
 c4 = data['S3AQ3C1'].value_counts(sort=False)
 print(c4)
 
 p4 = data['S3AQ3C1'].value_counts(sort=False, normalize=True)
-print (p4)
+print(p4)
 
 c4 = data['S3AQ3C1'].value_counts(sort=False)
 print(c4)
 
 p4 = data['S3AQ3C1'].value_counts(sort=False, normalize=True)
-print (p4)
+print(p4)
 
 #ADDING TITLES
 print 'counts for TAB12MDX'
 c1 = data['TAB12MDX'].value_counts(sort=False)
-print (c1)
+print(c1)
 #print (len(data['TAB12MDX'])) #number of observations (rows)
 
 print 'percentages for TAB12MDX'
 p1 = data['TAB12MDX'].value_counts(sort=False, normalize=True)
-print (p1)
+print(p1)
 
 print 'counts for CHECK321'
 c2 = data['CHECK321'].value_counts(sort=False)
@@ -67,7 +67,7 @@ print(c2)
 
 print 'percentages for CHECK321'
 p2 = data['CHECK321'].value_counts(sort=False, normalize=True)
-print (p2)
+print(p2)
 
 print 'counts for S3AQ3B1'
 c3 = data['S3AQ3B1'].value_counts(sort=False, dropna=False)
@@ -76,7 +76,7 @@ print(c3)
 
 print 'percentages for S3AQ3B1'
 p3 = data['S3AQ3B1'].value_counts(sort=False, normalize=True)
-print (p3)
+print(p3)
 
 print 'counts for S3AQ3C1'
 c4 = data['S3AQ3C1'].value_counts(sort=False, dropna=False)
@@ -84,16 +84,16 @@ print(c4)
 
 print 'percentages for S3AQ3C1'
 p4 = data['S3AQ3C1'].value_counts(sort=False, dropna=False, normalize=True)
-print (p4)
+print(p4)
 
 #ADDING MORE DESCRIPTIVE TITLES
 print 'counts for TAB12MDX – nicotine dependence in the past 12 months, yes=1'
 c1 = data['TAB12MDX'].value_counts(sort=False)
-print (c1)
+print(c1)
 
 print 'percentages for TAB12MDX nicotine dependence in the past 12 months, yes=1'
 p1 = data['TAB12MDX'].value_counts(sort=False, normalize=True)
-print (p1)
+print(p1)
 
 print 'counts for CHECK321 smoked in the past year, yes=1'
 c2 = data['CHECK321'].value_counts(sort=False)
@@ -101,7 +101,7 @@ print(c2)
 
 print 'percentages for CHECK321 smoked in the past year, yes=1'
 p2 = data['CHECK321'].value_counts(sort=False, normalize=True)
-print (p2)
+print(p2)
 
 print 'counts for S3AQ3B1 –usual frequency when smoked cigarettes'
 c3 = data['S3AQ3B1'].value_counts(sort=False)
@@ -109,7 +109,7 @@ print(c3)
 
 print 'percentages for S3AQ3B1 - usual frequency when smoked cigarettes'
 p3 = data['S3AQ3B1'].value_counts(sort=False, normalize=True)
-print (p3)
+print(p3)
 
 print 'counts for S3AQ3C1 usual quantity when smoked cigarettes'
 c4 = data['S3AQ3C1'].value_counts(sort=False)
@@ -117,32 +117,32 @@ print(c4)
 
 print 'percentages for S3AQ3C1 usual quantity when smoked cigarettes'
 p4 = data['S3AQ3C1'].value_counts(sort=False, normalize=True)
-print (p4)
+print(p4)
 
 # freqeuncy disributions using the 'bygroup' function
 ct1= data.groupby('TAB12MDX').size()
-print ct1
+print(ct1)
 
 pt1 = data.groupby('TAB12MDX').size() * 100 / len(data)
-print pt1
+print(pt1)
 
 ct2= data.groupby('CHECK321').size()
-print ct2
+print(ct2)
 
 pt2 = data.groupby('CHECK321').size() * 100 / len(data)
-print pt2
+print(pt2)
 
 ct3= data.groupby('S3AQ3B1').size()
-print ct3
+print(ct3)
 
 pt3 = data.groupby('S3AQ3B1').size() * 100 / len(data)
-print pt3
+print(pt3)
 
 ct4= data.groupby('S3AQ3C1').size()
-print ct4
+print(ct4)
 
 pt4 = data.groupby('S3AQ3C1').size() * 100 / len(data)
-print pt4
+print(pt4)
 
 #subset data to young adults age 18 to 25 who have smoked in the past 12 months
 sub1=data[(data['AGE']>=18) & (data['AGE']<=25) & (data['CHECK321']==1)]
@@ -157,7 +157,7 @@ print(c5)
 
 print 'percentages for AGE'
 p5 = sub2['AGE'].value_counts(sort=False, normalize=True)
-print (p5)
+print(p5)
 
 print 'counts for CHECK321'
 c6 = sub2['CHECK321'].value_counts(sort=False)
@@ -165,7 +165,7 @@ print(c6)
 
 print 'percentages for CHECK321'
 p6 = sub2['CHECK321'].value_counts(sort=False, normalize=True)
-print (p6)
+print(p6)
 
 #upper-case all DataFrame column names - place afer code for loading data aboave
 data.columns = map(str.upper, data.columns)
